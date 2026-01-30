d147d2b09a0c30d8f3e138a8368340444d9ffc90
stlmargy <stellahmargy0211@gmail.com>
2026-01-30 14:23:44 +0300

    installs flask_sqlalchemy and initializes it with flask app

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from .extensions import db
 
 def create_app():
     app=Flask(__name__)
+    # initialize the app with the extension
+    db.init_app(app)
     # a simple page that says hello
     @app.route('/hello')
     def hello():
