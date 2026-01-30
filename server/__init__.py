cc578c4fd0c539a62631ff2b12dcc327d9842215
stlmargy <stellahmargy0211@gmail.com>
2026-01-30 13:58:55 +0300

    Creates flask application factory - create_app fn

---
+++ server/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+def create_app():
+    app=Flask(__name__)
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Flask app is working !'
+    return app
