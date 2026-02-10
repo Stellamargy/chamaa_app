6c509ac42ce8e917e90940244fc71d32188021fe
stlmargy <stellahmargy0211@gmail.com>
2026-02-10 12:21:49 +0300

    defines user and base model

---
+++ server/models/basemodel.py
@@ -0,0 +1,15 @@
+from . import db
+from sqlalchemy import Integer,DateTime ,func
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+class BaseModel(db.Model):
+    __abstract__ = True  # This tells SQLAlchemy not to create a table for this class(only exist to be inherited from )
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    created_at: Mapped[datetime]=mapped_column(DateTime,nullable=False,server_default=func.now())
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime, 
+        nullable=True,  # Allow NULL
+        onupdate=func.now()  # Only set when actually updated
+    )
+
+    
--- server/models/user.py
+++ server/models/user.py
@@ -1,2 +1,12 @@
-class User:
-    pass
+from sqlalchemy import Integer, String,Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from .basemodel import BaseModel
+
+class User(BaseModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String, nullable=False)
+    email_address: Mapped[str] = mapped_column(String, nullable=False,unique=True)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    is_verified:Mapped[str]=mapped_column(Boolean,nullable=False,default=False)
+    
