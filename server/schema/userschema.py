7a8b255e75c24f08820bc4defd9dd18e5bfc4a82
stlmargy <stellahmargy0211@gmail.com>
2026-02-13 10:41:02 +0300

    refactor userschema code to make it DRY

--- server/schema/userschema.py
+++ server/schema/userschema.py
@@ -1,17 +1,40 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import fields,validate
 from server.models.user import User
 from marshmallow import ValidationError
 from marshmallow import validates, ValidationError
+from marshmallow_sqlalchemy import SQLAlchemySchema,auto_field
+# class UserSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = User
+#         exclude = ("password_hash","id","created_at","updated_at")
 
-class UserSchema(SQLAlchemyAutoSchema):
+#     first_name = fields.String(required=True)
+#     last_name = fields.String(required=True)
+#     password = fields.String(required=True, load_only=True, validate=validate.Length(min=10))
+#     email_address = fields.Email(required=True)
+
+   
+#     @validates("first_name")
+#     def validate_first_name(self, value,**kwargs):
+#         if not value.strip():
+#             raise ValidationError("First name cannot be blank.")
+
+#     @validates("last_name")
+#     def validate_last_name(self, value,**kwargs):
+#         if not value.strip():
+#             raise ValidationError("Last name cannot be blank.")
+
+
+
+class UserSchema(SQLAlchemySchema):
     class Meta:
         model = User
-        exclude = ("password_hash","id","created_at","updated_at")
+        # exclude = ("password_hash","id","created_at","updated_at")
 
     first_name = fields.String(required=True)
     last_name = fields.String(required=True)
-    password = fields.String(required=True, load_only=True, validate=validate.Length(min=10))
+    password = fields.String(required=True, load_only=True, validate=validate.Length(min=6))
     email_address = fields.Email(required=True)
 
    
@@ -24,4 +47,3 @@ class UserSchema(SQLAlchemyAutoSchema):
     def validate_last_name(self, value,**kwargs):
         if not value.strip():
             raise ValidationError("Last name cannot be blank.")
-
