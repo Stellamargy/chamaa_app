4f58ad58ce7fa3e154e4311e4a46014ca4728948
stlmargy <stellahmargy0211@gmail.com>
2026-02-17 12:26:06 +0300

    clean up

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,28 +1,9 @@
-# from flask import Blueprint
-# from flask_restful import Api
-# from server.resources.auth import RegisterResource,LoginResource
 from server.repositories.user_repository import UserRepository
 from server.services.user_registration import UserRegistration
 from server.utilis.api_response import ApiResponse
 from server.models import db  
 from flask import Blueprint, request
 from marshmallow import ValidationError
-
-
-# auth_bp=Blueprint("auth",__name__)
-# api=Api(auth_bp)
-# # Set up dependecies 
-# user_repository=UserRepository(db.session)
-# user_registration_service=UserRegistration(user_repository)
-
-
-# api.add_resource(
-#     RegisterResource,
-#     "/sign_up",
-#     resource_class_kwargs={'user_registration_service': user_registration_service}
-# )
-# api.add_resource(LoginResource,"/sign_in")
-
 
 auth_bp = Blueprint("auth", __name__)
 
