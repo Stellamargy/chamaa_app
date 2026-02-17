0bf55cd89a94968dd277b3c555a8a7cbfef264c8
stlmargy <stellahmargy0211@gmail.com>
2026-02-17 11:59:43 +0300

    adds sucess and error response method for api response consistency

--- server/utilis/api_response.py
+++ server/utilis/api_response.py
@@ -1,2 +1,19 @@
+from flask import jsonify
 class ApiResponse:
-    pass
+    def success(self,data=None, message=None, status_code=200):
+        response = {
+            "success": True,
+            "message": message,
+            "data": data,
+            "errors": None
+        }
+        return jsonify(response), status_code
+
+    def error(self, message=None, errors=None, status_code=400):
+        response = {
+            "success": False,
+            "message": message,
+            "data": None,
+            "errors": errors
+        }
+        return jsonify(response), status_code    
