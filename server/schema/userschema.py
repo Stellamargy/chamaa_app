7ff93a52502112a7207e2853fb5db0cab3cfb020
stlmargy <stellahmargy0211@gmail.com>
2026-03-03 13:19:34 +0300

    added validation for phone_number field -it should be a valid phone number added validation for password field - ensures password are strong and difficult to guess

--- server/schema/userschema.py
+++ server/schema/userschema.py
@@ -1,37 +1,63 @@
-# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow import fields,validate
+import re
+from marshmallow import fields, validate, ValidationError, pre_load, validates
+from marshmallow_sqlalchemy import SQLAlchemySchema
 from server.models.user import User
-from marshmallow import validates, ValidationError,pre_load
-from marshmallow_sqlalchemy import SQLAlchemySchema,auto_field
+
 
 class UserSchema(SQLAlchemySchema):
     class Meta:
         model = User
-        # exclude = ("password_hash","id","created_at","updated_at")
+    #Fields to validate
+    first_name = fields.String(required=True, allow_none=False)
+    last_name = fields.String(required=True, allow_none=False)
+    password = fields.String(
+        required=True,
+        allow_none=False,
+        load_only=True,
+        validate=validate.Length(min=6)
+    )
 
-    first_name = fields.String(required=True)
-    last_name = fields.String(required=True)
-    password = fields.String(required=True, load_only=True, validate=validate.Length(min=6))
-    email_address = fields.Email(required=True)
+    email_address = fields.Email(required=True, allow_none=False)
+    phone_number = fields.String(required=True, allow_none=False)
 
-    #Run this fn to normalize email address before schema level validation 
+    
     @pre_load
-    def normalize_email_address(self,user_input,**kwargs):
+    def normalize_email_address(self, user_input, **kwargs):
         if "email_address" in user_input:
-            user_input["email_address"]=user_input["email_address"].lower().strip()
+            user_input["email_address"] = user_input["email_address"].lower().strip()
         return user_input
-   
+
+    #Custom validations 
     @validates("first_name")
-    def validate_first_name(self, value,**kwargs):
+    def validate_first_name(self, value, **kwargs):
         if not value.strip():
             raise ValidationError("First name cannot be blank.")
 
     @validates("last_name")
-    def validate_last_name(self, value,**kwargs):
+    def validate_last_name(self, value, **kwargs):
         if not value.strip():
             raise ValidationError("Last name cannot be blank.")
 
     @validates("password")
-    def validate_password(self, value,**kwargs):
+    def validate_password(self, value, **kwargs):
         if not value.strip():
-            raise ValidationError("Password  cannot be blank.")        
+            raise ValidationError("Password cannot be blank.")
+
+        pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$"
+        if not re.match(pattern, value):
+            raise ValidationError(
+                "Password must contain at least one uppercase letter, "
+                "one lowercase letter, one number, and one symbol."
+            )
+
+    @validates("phone_number")
+    def validate_phone_number(self, value, **kwargs):
+        if not value.strip():
+            raise ValidationError("Phone number cannot be blank.")
+
+        pattern = r"^\+254(7|1)\d{8}$"
+        if not re.match(pattern, value):
+            raise ValidationError(
+                "Phone number must be a valid Kenyan number starting with +25 and 12 digits in total"
+            )            
+
