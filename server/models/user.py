6714c160bfc492d197d3c3743ed530c469893915
stlmargy <stellahmargy0211@gmail.com>
2026-03-03 12:24:23 +0300

    adds phone_number and is_active columns or attributes to user model.

--- server/models/user.py
+++ server/models/user.py
@@ -5,17 +5,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(BaseModel):
     __tablename__="users"
-    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    email_address: Mapped[str] = mapped_column(String(100), nullable=False,unique=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email_address: Mapped[str] = mapped_column(String(50), nullable=False,unique=True)
+    #Targeted for Kenyan users 
+    phone_number:Mapped[str]=mapped_column(String(13),nullable=False,unique=True)
     _password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
-    is_verified:Mapped[str]=mapped_column(Boolean,nullable=False,default=False)
+    is_verified:Mapped[bool]=mapped_column(Boolean,nullable=False,default=False)
+    is_active:Mapped[bool]=mapped_column(Boolean,nullable=False,default=True)
 
-
+    #getter for _password_hash
     @property
     def password(self):
-        raise AttributeError("Password is write-only")  # Cannot read
-    # Hash password from plain text when creating a user instance - security  
+        return self._password_hash
+    #setter for _password_hash - hashes plain text password for security 
     @password.setter
     def password(self, plain_text):
         self._password_hash = generate_password_hash(plain_text)
