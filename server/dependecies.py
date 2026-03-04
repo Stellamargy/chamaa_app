07fc7acb449afb1a0cdaac15707df83338eb27c1
stlmargy <stellahmargy0211@gmail.com>
2026-03-04 19:06:13 +0300

    clean up

--- server/dependecies.py
+++
@@ -1,7 +0,0 @@
-from server.models import db
-from server.repositories.user_repository import UserRepository
-from server.services.user_registration_service import UserRegistrationService
-
-def get_user_registration_service():
-    repo = UserRepository(db.session)  
-    return UserRegistrationService(repo)
