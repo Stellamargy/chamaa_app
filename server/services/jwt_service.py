088d0aa196b487290e0866acf76792354afc5410
stlmargy <stellahmargy0211@gmail.com>
2026-03-13 13:13:34 +0300

    Raised correct business exception, refactored and cleaned code

--- server/services/jwt_service.py
+++ server/services/jwt_service.py
@@ -1,27 +1,24 @@
 import jwt
 from jwt import ExpiredSignatureError
+from server.exceptions import VerificationError
 from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token
 from flask import current_app
 
 
 class JwtService:
-    # Create a JWT access token for the given user.
+    # Create  access token
     @staticmethod
     def generate_access_token(user):
-        try:
-            return create_access_token(
+        return create_access_token(
                 identity=str(user.id),
                 additional_claims={
                     "email_verified": user.is_verified,
                     "user_active": user.is_active,
                 },
             )
-
-        except Exception as e:
-            # preserve original error for logging
-            raise RuntimeError("Failed to generate access token") from e
-
+        
+            
     # Create email verification token
     @staticmethod
     def create_email_verification_token(user_id):
@@ -29,19 +26,16 @@ class JwtService:
         payload = {
             "sub": str(user_id),
             "exp": datetime.now(tz=timezone.utc) + timedelta(hours=24),
-            "iat": datetime.now(tz=timezone.utc),
             "type": "email_verification",
         }
-
-        try:
-            token = jwt.encode(
+        token = jwt.encode(
                 payload,
                 current_app.config["EMAIL_VERIFICATION_SECRET"],
                 algorithm="HS256",
             )
-            return token
-        except Exception as e:
-            raise RuntimeError("Failed to generate access token") from e
+        return token
+
+       
     # Checks email verification token validility   
     @staticmethod
     def decode_email_verification_token(token):
@@ -53,12 +47,12 @@ class JwtService:
             )
 
             if payload.get("type") != "email_verification":
-                raise ValueError("Invalid token type")
+                raise VerificationError("Invalid token type")
 
             return payload
 
         except jwt.ExpiredSignatureError as e:
-            raise AuthenticationError("Verification link expired") from e
+            raise VerificationError("Verification link has expired") from e
 
         except jwt.InvalidTokenError as e:
-            raise AuthenticationError("Invalid verification token") from e
+            raise VerificationError("Invalid verification token") from e
