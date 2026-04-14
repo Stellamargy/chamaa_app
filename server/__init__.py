c4bf07e905bdc4263ec8ba9ded8f90fc9f6c6ad4
stlmargy <stellahmargy0211@gmail.com>
2026-04-14 16:59:11 +0300

    creates chama blueprint and registers it-flask app instance

--- server/__init__.py
+++ server/__init__.py
@@ -3,6 +3,7 @@ from server.models import db
 from server.models.user import User
 from .config import Config 
 from server.routes.auth import auth_bp
+from server.routes.chama import chama_bp
 from .extensions import jwt
 from server.errors.error_handlers import register_error_handlers
 
@@ -19,7 +20,8 @@ def create_app():
     jwt.init_app(app)   
 
     #register bluerints 
-    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    app.register_blueprint(chama_bp,url_prefix="/api/chamas")
     # register error handler
     register_error_handlers(app)
 
---
+++ server/routes/chama.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+#create chama blueprint
+chama_bp = Blueprint("chamas", __name__, url_prefix="/api/chamas")
