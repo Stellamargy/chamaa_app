255c8728fd3203af16547efe0149cb53ca93036d
stlmargy <stellahmargy0211@gmail.com>
2026-04-18 10:56:04 +0300

    installed and  flask migrate

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,7 @@ from server.models.user import User
 from .config import Config 
 from server.routes.auth import auth_bp
 from server.routes.chama import chama_bp
-from .extensions import jwt
+from .extensions import jwt ,migrate
 from server.errors.error_handlers import register_error_handlers
 
 def create_app():
@@ -17,11 +17,12 @@ def create_app():
 
     # initialize extensions 
     db.init_app(app)
-    jwt.init_app(app)   
+    jwt.init_app(app)
+    migrate.init_app(app=app,db=db) 
 
     #register bluerints 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
-    app.register_blueprint(chama_bp,url_prefix="/api/chamas")
+    app.register_blueprint(chama_bp)
     # register error handler
     register_error_handlers(app)
 
--- server/extensions.py
+++ server/extensions.py
@@ -1,3 +1,5 @@
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 #jwt object
 jwt = JWTManager()
+migrate=Migrate()
